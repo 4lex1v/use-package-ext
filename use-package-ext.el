bd616b520ea7ebaa010ea05a024901776d628095
Aleksandr Ivanov <4lex1v@gmail.com>
2017-01-08 22:56:05 +0300

    Working version

--- use-package-ext.el
+++ use-package-ext.el
@@ -1,8 +1,7 @@
 (require 'use-package)
-(require 'async)
+(require 'deferred)
 
-(defvar use-package-sources-cache-folder
-  (expand-file-name ".cached-packages" user-emacs-directory)
+(defvar use-package-sources-cache-folder ".cached-packages"
   "Folder in which all source downloaded by use-package are stored")
 
 (defun insert-after (list elem insert-elem)
@@ -11,26 +10,47 @@
         ((eq elem (car list)) (cons elem (cons insert-elem (cdr list))))
         (t (cons (car list) (insert-after (cdr list) elem insert-elem)))))
 
-(defun use-package-normalize/:github (package-name keyword args)
-  (use-package-only-one (symbol-name keyword) args
-    (lambda (label arg) arg)))
+(defun use-package-ext-tripple (args f)
+  (declare (indent 1))
+  (apply f (pcase args
+             (`((,repo . ,out) ,asyncp . ,_) (list repo out t))
+             (`(,repo ,asyncp . ,_) (list repo nil t))
+             (`((,repo . ,out) . ,_) (list repo out nil))
+             (`(,repo) (list repo nil nil)))))
 
-(defun use-package-handler/:github (package-name-s keyword source-type-arg rest state)
-  (let* ((body (use-package-process-keywords package-name-s rest state))
-         (package-name (symbol-name package-name-s))
-         (path-in-cache-folder (expand-file-name package-name use-package-sources-cache-folder)))
+(defun if-nil-then (arg then)
+  (if (not arg) then arg))
+
+(defun use-package-normalize/:github (package-name-s keyword args)
+  (when (> (length args) 2)
+    (use-package-error ":github accepts no more then two args"))
+  (use-package-ext-tripple args
+    (lambda (repo out-folder asyncp)
+      (list
+       (if (cl-search "/" (symbol-name repo))
+           (symbol-name repo)
+         (format "%s/%s.el" repo package-name-s))
+       (if-nil-then out-folder use-package-sources-cache-folder)
+       asyncp))))
+
+(defun use-package-handler/:github (package-name-s keyword config rest state)
+  (pcase-let* ((`(,repo ,output-path ,asyncp . ,_) config)
+               (package-name (symbol-name package-name-s))
+               (destination-folder (expand-file-name (concat output-path "/" package-name)
+                                                     user-emacs-directory))
+               (monkey (append `(:load-path ,(list destination-folder)) rest))
+               (body (use-package-process-keywords package-name-s monkey state)))
     (use-package-concat
-     `((if (file-exists-p ,path-in-cache-folder)
-           (progn
-             (add-to-list 'load-path ,path-in-cache-folder)
-             ,@body)
-         ,(let ((command (format "git clone git@github.com:%s/%s.git %s"
-                                 source-type-arg
-                                 package-name
-                                 (expand-file-name package-name use-package-sources-cache-folder))))
-            `(async-start
-              (lambda () (shell-command ,command))
-              (lambda (result) ,@body))))))))
+     `((if (file-exists-p ,destination-folder)
+           (progn ,@body)
+         (let ((git-clone-args (format "git@github.com:%s.git %s" ,repo ,destination-folder)))
+           ,(if asyncp
+                `(deferred:$
+                   (deferred:process-shell "git" "clone" git-clone-args)
+                   (deferred:nextc it (lambda (result) ,@body)))
+              `(progn
+                 (shell-command (format "git clone %s" git-clone-args))
+                 ,@body))))))))
 
 (setq use-package-keywords (insert-after use-package-keywords :requires :github))
 
